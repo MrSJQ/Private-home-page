145f70cc77988ddde1fb1e6120b5e4730a45f389
MrSJQ <326336467@qq.com>
2017-05-22 14:42:55 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,19 +1,26 @@
-$(document).ready(function(){
-	toggletip();
-})
-function toggletip(){
+//$(document).ready(function(){
+//	toggletip();
+//})
+//function toggletip(){
 	$(".box-second-one").hover(function(){
-		$(this).find(".firsttip").show(500);
-		$(this).find(".secondtip").show(1000);
+		$(this).find(".firsttip").show(300);
+		$(this).find(".secondtip").show(500);
 	},function(){
-		$(this).find(".firsttip").hide(500);
-		$(this).find(".secondtip").hide(1000);
+		$(this).find(".firsttip").hide(300);
+		$(this).find(".secondtip").hide(500);
 	})
 	$(".box-second-three").hover(function(){
-		$(this).find(".firsttip").show(500);
-		$(this).find(".secondtip").show(1000);
+		$(this).find(".firsttip").show(300);
+		$(this).find(".secondtip").show(500);
 	},function(){
-		$(this).find(".firsttip").hide(500);
-		$(this).find(".secondtip").hide(1000);
+		$(this).find(".firsttip").hide(300);
+		$(this).find(".secondtip").hide(500);
 	})
-}
+	$(".box-fourth-div").hover(function(){
+		$(this).find(".firsttip").show(300);
+		$(this).find(".secondtip").show(500);
+	},function(){
+		$(this).find(".firsttip").hide(300);
+		$(this).find(".secondtip").hide(500);
+	})
+//}
